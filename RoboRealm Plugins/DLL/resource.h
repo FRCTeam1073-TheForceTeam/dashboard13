bb05a7ed1fea7d9e5b27e2618b423f12be9a8a2a
Greg C <cowang4@yahoo.com>
2013-01-21 13:05:47 -0500

    RoboRealm Plugins!

---
+++ RoboRealm Plugins/DLL/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by RRModule.rc
+//
+#define IDC_SHOW_HELP                   3
+#define IDD_SWAPCOLOR                   4000
+#define IDC_MYTEXT                      4000
+#define IDC_SWAP_RB                     4001
+#define IDC_SWAP_RG                     4002
+#define IDC_SWAP_GB                     4003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        4001
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         4002
+#define _APS_NEXT_SYMED_VALUE           4000
+#endif
+#endif
