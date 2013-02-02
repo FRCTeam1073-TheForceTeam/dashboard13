e9cca9e62f05cf00a95d5d2101dd0a6b6c485790
Greg C <cowang4@yahoo.com>
2013-02-02 16:56:21 -0500

    more work on nettables and vision

--- Vision Extension/src/team1073/smartdashboard/extension/roborealmapi/VisionProccesing.java
+++ Vision Extension/src/team1073/smartdashboard/extension/roborealmapi/VisionProccesing.java
@@ -4,6 +4,7 @@
  */
 package team1073.smartdashboard.extension.roborealmapi;
 
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import java.awt.image.BufferedImage;
 
 /**
@@ -15,17 +16,24 @@ public class VisionProccesing {
     BufferedImage image;
     RR_API api;
     double[] variables;
+    NetworkTable visionTable;
+    double currentAngle;
+    double currentSpeed;
     
     VisionProccesing()
     {
-        //add constructor code here if neccessary
+        NetworkTable.setTeam(1073);
+        NetworkTable.setIPAddress("10.10.73.2");
+        visionTable = NetworkTable.getTable("tracking");
+        
     }
 
     public BufferedImage proccessImage(BufferedImage rawImage, RR_API roboApi)
     {
         image = rawImage; //not neccesary, but ok as a backup in case we want to use in different functions
         api = roboApi;
-        variables = getRRVariables();
+        variables = getRRVariables();//gets roboRealm's variables, see below
+        getCurrentValues();//gets actual valus from robot, see below
         
         //do processing and image overlaying here
         
@@ -44,6 +52,24 @@ public class VisionProccesing {
         
         return values;
     }
+    
+    private void getCurrentValues()
+    {
+        try
+        {
+            currentAngle = visionTable.getNumber("currentAngle");
+            currentSpeed = visionTable.getNumber("currentSpeed");
+        }
+        catch(Exception e){System.out.println("Exception in Network Table getNumber" + e);}
+        
+        System.out.println("currentAngle: " + currentAngle + "   currentSpeed: " + currentSpeed);
+        System.out.println("Is Connected: " + visionTable.isConnected());
+    }
             
+    private void sendCalcValues(double angle, double distance)
+    {
+        visionTable.putNumber("calcAngle", angle);
+        visionTable.putNumber("calcDistance", distance);
+    }
     
 }
