d049f77b6691380afc999ab04d63ec2b9bd10120
Devin Orr <devin6247@gmail.com>
2013-02-16 14:22:00 -0500

    Refer to Previous Commit

--- WidgetSource/GUIWidgets/src/team1073/smartdashboard/extension/guiwidgets/speedDisplay.java
+++ WidgetSource/GUIWidgets/src/team1073/smartdashboard/extension/guiwidgets/speedDisplay.java
@@ -34,7 +34,7 @@ public final DoubleProperty newVal = new DoubleProperty(this, "Calc Val", 0.0);
 
     @Override
     public void init() {
-        setPreferredSize(new Dimension(130, 280));
+        setPreferredSize(new Dimension(150, 280));
     }
     
     @Override
