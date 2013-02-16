5907913bfd23515f2982a963f33d530a11b5d088
Keenan Troddyn <keenan.troddyn@gmail.com>
2013-02-15 22:30:41 -0500

    Worked on Speedometer (INCOMPLETE)

--- WidgetSource/GUIWidgets/src/team1073/smartdashboard/extension/guiwidgets/Speedometer.java
+++ WidgetSource/GUIWidgets/src/team1073/smartdashboard/extension/guiwidgets/Speedometer.java
@@ -13,20 +13,25 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
+import java.lang.Math;
 /**
  *
  * @author Kate
+ * Nick Also Helped :)
  */
 public class Speedometer extends StaticWidget{
+    private final int maxSpeed = 20;
+    private final int minSpeed = 0;
     private int speed;
     public final IntegerProperty newVal = new IntegerProperty(this, "Speed", 0);
     public static final DataType[] TYPES = {DataType.NUMBER};
     
+    @Override
     public void init() {
         speed = 0;
         setPreferredSize(new Dimension(200,150));
     }
+    
     
     @Override
     protected void paintComponent(Graphics g) {
@@ -34,8 +39,20 @@ public class Speedometer extends StaticWidget{
         g2.setColor(Color.BLACK);
         g2.setStroke(new BasicStroke(3.0f));
         g2.drawLine(0,150, 150, 150);
-        
-        if(Math.abs(speed) > 3500) {
+        g2.setColor(Color.BLACK);
+        Color c = new Color(255, 255, 255);
+        g.setColor(c);
+        Color d = new Color(255, 200, 200);
+        g.setColor (d);
+        Color e = new Color(255, 230, 230);
+        g.setColor (e);
+        int radius = 150;
+        g2.drawArc(0, 75, radius, radius, -180, -180);
+        float angle = (speed / (maxSpeed - minSpeed) * 180);
+        float endpointx = (float)(Math.cos(Math.toRadians(angle)) * radius/2);
+        float endpointy = (float)(Math.sin(Math.toRadians(angle)) * radius/2);
+
+        if(Math.abs(speed) > 50) {
             g2.setColor(Color.RED);
             g2.fillArc(0, 75, 150, 150, 0, 180);
         }
@@ -54,9 +71,12 @@ public class Speedometer extends StaticWidget{
     public void setValue(Object o) {
         int d = speed;
        
-       try{d = ((Number)o).intValue();;}
-       catch(Exception e){}
-       
+       try{d = ((Number)o).intValue();}
+       catch(Exception e){
+           
+           System.out.println("Speedometer setValue exception " + e);
+       }
+              
        speed = d;
        
        repaint();
