d6119c8552f9c73476af32fcc32318164e1957a5
Devin Orr <devin6247@gmail.com>
2013-02-16 16:08:06 -0500

    Put Limit in speedDisplay

--- WidgetSource/GUIWidgets/src/team1073/smartdashboard/extension/guiwidgets/speedDisplay.java
+++ WidgetSource/GUIWidgets/src/team1073/smartdashboard/extension/guiwidgets/speedDisplay.java
@@ -33,7 +33,7 @@ public final DoubleProperty newVal = new DoubleProperty(this, "Calc Val", 0.0);
 
     @Override
     public void init() {
-        setPreferredSize(new Dimension(150, 280));
+        setPreferredSize(new Dimension(170, 280));
     }
     
     @Override
@@ -49,20 +49,29 @@ public final DoubleProperty newVal = new DoubleProperty(this, "Calc Val", 0.0);
     int height = 200;
     
     int barHeight = (int)(height * actualValue/100.0);
+        if (barHeight <= 200 && barHeight >= -100) { //sets limit to 100
+            g.setColor(Color.BLACK);
+            g.fillRect(50, 50, 64, 200);
     
-    g.setColor(Color.BLACK);
-    g.fillRect(50, 50, 64, 200);
+            g.setColor(Color.GREEN);
+            g.fillRect(50, 250 - barHeight, 64, barHeight);
     
-    g.setColor(Color.GREEN);
-    g.fillRect(50, 250 - barHeight, 64, barHeight);
+            g.setColor(Color.BLACK);
+            g.setStroke(new BasicStroke (2.0f));
+            g.drawRect(50, 50, 64 - 1, 200 - 1);
     
-    g.setColor(Color.BLACK);
-    g.setStroke(new BasicStroke (2.0f));
-    g.drawRect(50, 50, 64 - 1, 200 - 1);
-    
-    Font font = new Font("Arial", Font.BOLD, 16);
-    graphics.setFont(font);
-    g.drawString("Speed: " + Integer.toString(Math.abs((int)actualValue)), 47, 272);
+            Font font = new Font("Arial", Font.BOLD, 16);
+            graphics.setFont(font);
+            g.drawString("Speed: " + Integer.toString(Math.abs((int)actualValue)), 47, 272);
     }
    
+        else { 
+            g.setColor(Color.RED);
+            g.fillRect(50, 50, 64, 200);
+            
+            Font font = new Font("Arial", Font.BOLD, 16);
+            graphics.setFont(font);
+            g.drawString("INVALID SPEED", 27, 272);
+        }
+    }
 }
